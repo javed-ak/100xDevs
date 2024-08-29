170aa78048459fc899817b4196475b7a18e3fe9d
Javed Akhtar <conn.javed@gmail.com>
2024-08-29 17:31:27 +0530

    week-7.4.4-atomFamily-selectorFamily

--- Week-7/week-7.4.4/src/App.jsx
+++ Week-7/week-7.4.4/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { todosAtomFamily } from './atoms'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <RecoilRoot>
+      <Todo id = {1} />
+      <Todo id = {4} />
+  </RecoilRoot>
 }
 
+function Todo({id}) {
+  const currentTodo = useRecoilValue(todosAtomFamily(id))
+  return <>
+  <h3>{currentTodo.title}</h3>
+  <h3>{currentTodo.description}</h3>
+  </>
+}
 export default App
