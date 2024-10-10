32325d71c4f19d49a107deec355fef0e3c55a4cb
Javed Akhtar <conn.javed@gmail.com>
2024-10-10 10:11:30 +0530

    week-4.1.2

---
+++ Week-4/week-4.1.2/backend.js
@@ -0,0 +1,22 @@
+const express = require ("express");
+const cors = require ("cors")
+const app = express();
+
+
+app.use(cors());
+
+app.get("/interest", (req, res) => {
+    const principal = parseInt(req.query.principal);
+    const rate = parseInt(req.query.rate);
+    const time = parseInt(req.query.time);
+
+    const interest = (principal * rate * time) / 100;
+    const total = principal + interest;
+
+    res.send({
+        Total: total,
+        Interest: interest
+    });
+})
+
+app.listen(3000)
