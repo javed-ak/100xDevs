c5d8b2c41c3d7f201b690a2ab517f40d4948616a
Javed Akhtar <conn.javed@gmail.com>
2024-10-18 20:36:41 +0530

    week-11

--- Week-11/my-app/src/index.ts
+++ Week-11/my-app/src/index.ts
@@ -1,6 +1,19 @@
 
+// export default {
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
-} satisfies ExportedHandler<Env>;
+};
