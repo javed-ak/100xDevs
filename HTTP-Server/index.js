46b7657390695c4b5f0bd96ecf3a5c1f34eed911
Javed Akhtar <conn.javed@gmail.com>
2025-04-25 16:25:06 +0530

    file updated

---
+++ HTTP-Server/index.js
@@ -0,0 +1,44 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send("Hii there, This the home page. And you're most welcome here")
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send("Javed is a passionate Full-stack Developer with expertise in MERN Stack Development.")
+// })
+
+// app.listen(3000);
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//     setTimeout(() => {
+//         res.send("This is after 5 sec")
+//     }, 5000);
+//     res.send("Hii there Javed")
+// })
+
+// app.listen(3000)
+
+// const express = require('express');
+// const app = express();
+
+
+
+// const getSum = (input) => {
+//     let sum = 0;
+//     for(let i=0; i<input; i++) {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+
+// app.get('/', (req, res) => {
+//     const a = req.query.a;
+//     const totalSum = getSum(a);
+//     res.send("Totol sum of the number you given is "+ totalSum)
+// })
+// app.listen(3000)
